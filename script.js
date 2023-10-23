af3c5f019232a79d632eda4d1b0a2835e71e931d
RyanHazlewood0 <114792045+RyanHazlewood0@users.noreply.github.com>
2023-10-24 09:58:54 +1300

    toggleDone function partly done

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let myToDos = []
 
+//base variables
 let add = document.getElementById('add-to-do')
 let modal = document.getElementById('modal')
 let toDoContainer = document.getElementById('to-do-container')
@@ -16,15 +17,18 @@ let descriptionLabel = document.getElementById('description-label')
 let dateLabel = document.getElementById('date-label')
 let priorityLabel = document.getElementById('priority-label')
 
+//toDo class
 class toDo {
-  constructor(title, description, dueDate, priority) {
+  constructor(title, description, dueDate, priority, isDone = false) {
 this.title = title
 this.description = description
 this.dueDate = dueDate
 this.priority = priority
+this.isDone = isDone
   }
 }
 
+//show / hide modal
 add.addEventListener('click', function() {
 modal.style.display = 'inline'
 })
@@ -33,24 +37,29 @@ closeModal.addEventListener('click', function() {
   modal.style.display = 'none'
 })
 
+//displayTodos function
 function displayToDos() {
   toDoContainer.innerHTML = ''
   
-myToDos.forEach(function(toDo, index,) {
+myToDos.forEach(function(toDo, index) {
 
   const toDoEntry = document.createElement('div')
   toDoEntry.id = 'toDoEntry'
   toDoEntry.innerHTML = 
   `
+  <div class="to-do-card">
   ${titleLabel.textContent} ${toDo.title}<br>
   <div class="to-do-btns-container">
-    <button class="expand-collapse-btn">expand / collapse details</button><br>
+    <button class="expand-collapse-btn">show / hide details</button><br>
     <button class="delete-to-do-btn" data-index="${index}">delete</button>
+    <label for="done" id="done-label">Task done?</label>
+    <input type="checkbox" name="done" id="done" data-index="${index}">
   </div>
   <div class="expand-collapse-content" style="display: none;">
     ${descriptionLabel.textContent} ${toDo.description}<br> 
     ${dateLabel.textContent}  ${toDo.dueDate}<br>
-    ${priorityLabel.textContent}  ${toDo.priority}
+    ${priorityLabel.textContent}  ${toDo.priority}<br>
+  </div>
   </div>
 `
 const deleteButton = toDoEntry.querySelector('.delete-to-do-btn');
@@ -62,14 +71,13 @@ const deleteButton = toDoEntry.querySelector('.delete-to-do-btn');
 
     toDoContainer.appendChild(toDoEntry)
 
-  titleInput.value = ''
+    titleInput.value = ''
   descriptionInput.value = ''
   dateInput.value = ''
   priorityInput.value = ''
   modal.style.display = 'none'
 })
-
-  let expandCollapseBtns = document.getElementsByClassName('expand-collapse-btn')
+let expandCollapseBtns = document.getElementsByClassName('expand-collapse-btn')
 let expandCollapseContent = document.getElementsByClassName('expand-collapse-content')
 
 for(let i = 0; i < expandCollapseBtns.length; i++)
@@ -80,12 +88,36 @@ for(let i = 0; i < expandCollapseBtns.length; i++)
       expandCollapseContent[i].style.display = 'none'
     }
   })
+  
+  function toggleDone(toDoIndex) {
+    const toDo = myToDos[toDoIndex];
+    toDo.isDone = !toDo.isDone;
+  }
+
+  function toggleDisplay() {
+    if (doneInput.checked) {
+      doneLabel.textContent = "Task is done"
+    } else {
+      doneLabel.textContent = "Task is not done"
+    }
+  }
+
+  let doneInput = document.getElementById('done')
+  let doneLabel = document.getElementById('done-label')
+
+  doneInput.addEventListener('change', function() {
+    const toDoIndex = parseInt(this.getAttribute('data-index'));
+    toggleDone(toDoIndex)
+    toggleDisplay()
+  })
+
 }
 
+//submit form event listener
 modalForm.addEventListener('submit', function(event) {
   event.preventDefault()
   const newToDo = new toDo(titleInput.value, descriptionInput.value, dateInput.value, priorityInput.value)
   myToDos.push(newToDo)
 displayToDos()
 })
-  
+
